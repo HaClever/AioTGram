7090652536439517c0fe56af8c2d1ba586875e0d
mkbeh <91bb64437ac5ab5148b76b33efb218f4ef092a2c>
2020-08-02 03:56:52 +0300

    minor fixes

--- examples/webhook_examples/fastapi_example.py
+++ examples/webhook_examples/fastapi_example.py
@@ -22,7 +22,7 @@ async def handler(request: Request):
     request_body_dict = await request.json()
     update = await aiotgram.types.Update.de_json(request_body_dict)
 
-    return JSONResponse({"status": "success"})
+    return JSONResponse({"status": "success"}, status_code=200)
 
 
 def get_routes():
